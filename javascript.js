f8e2b61727b0ea1ceea8ed276448f85acb2978f5
HoustonSprouse919 <89546823+HoustonSprouse919@users.noreply.github.com>
2023-04-25 14:29:46 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,22 +6,51 @@ function node(xCoord,yCoord,knightStat=false,travelStat=false){
         traveld:travelStat
     }
 }
-let squares =[];
 function createBoard(){
+        let squares =[];
     for(let i=0;i<8;i++ ){
         for(let j=0; j<8; j++){
             squares.push(node(i,j))
         }
     }
+    return squares;
 }
-function createKnight(x,y){
+function createKnight(board,x,y){
     if(x==null || y==null){
-        squares[0].knight = true;
+        board[0].knight = true;
+        return board[0]
     }else{
         let index = ((x*8)+y);
-        squares[10].knight= true;
+        board[index].knight= true;
+        return board[index]
     }
 }
-createBoard();
-createKnight(1,2);
-console.log(squares)
+function buildTree(array){
+    let start =0;
+    let end = array.length -1;
+    if(start>end){
+        return null;
+    }
+    let middle = Math.ceil((start + end) / 2);
+  let rootNode= array[0];
+  return rootNode;
+}
+
+class knightsTravalis{
+    constructor(x,y){
+        this.gameBoard = createBoard();
+        this.knightLoc = createKnight(this.gameBoard,x,y)
+    }
+    info(){
+        console.log(this.gameboard);
+        console.log(knightLoc);
+    }
+    knightMoves(current = this.knightLoc){
+            let tempArray = [];
+            tempArray.push(current);
+            this.root = buildTree(tempArray);
+    }
+}
+let game =new knightsTravalis();
+game.knightMoves();
+console.log(game.root)
